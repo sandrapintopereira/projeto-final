cf234094aaef0d77a0df69182c83ca8eef87e449
Sandra Pereira <sandrajesuspintopereira@gmail.com>
2026-03-04 11:43:35 +0000

    rotas criadas

--- meu-projeto/src/app/app.routes.ts
+++ meu-projeto/src/app/app.routes.ts
@@ -1,5 +1,12 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './dashboard/dashboard';
+import { ListaMedia } from './lista-media/lista-media';
+import { DetalhesMedia } from './detalhes-media/detalhes-media';
+import { FormularioMedia } from './formulario-media/formulario-media';
 
 export const routes: Routes = [
-    //colocar rotas para cada componente
+    {path: '', component: Dashboard},
+    {path: 'media', component: ListaMedia},
+    {path: 'media/:id', component: DetalhesMedia},
+    {path: 'media/new', component: FormularioMedia},
 ];
