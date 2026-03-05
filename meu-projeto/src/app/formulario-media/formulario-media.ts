efd916f7061da6c587a7dcfdeff466914becd0d7
Sandra Pereira <sandrajesuspintopereira@gmail.com>
2026-03-05 12:26:51 +0000

    feat: implement form validation and submission logic in FormularioMedia component

--- meu-projeto/src/app/formulario-media/formulario-media.ts
+++ meu-projeto/src/app/formulario-media/formulario-media.ts
@@ -1,9 +1,38 @@
 import { Component } from '@angular/core';
+import { ConteudoMediaService } from '../conteudo-service';
+import { FormConteudo } from '../interfaces/formConteudo';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-formulario-media',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './formulario-media.html',
   styleUrl: './formulario-media.css',
 })
-export class FormularioMedia {}
+export class FormularioMedia {
+  constructor(private service: ConteudoMediaService,
+    private router: Router,
+  ) {}
+
+  form = new FormGroup({
+    titulo: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    tipo: new FormControl('filme', [Validators.required]),
+    estado: new FormControl('visto', [Validators.required]),
+    avaliacao: new FormControl(0),
+    genero: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    criador: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    anoLancamento: new FormControl(2026, [Validators.required]),
+    anoFim: new FormControl(null),
+  })
+
+
+  onSubmit() {
+    if(this.form.valid) {
+      this.service.adicionar(this.form.value as FormConteudo);
+      this.router.navigate(['media/lista']);
+    } else {
+      alert('Por favor, preencha o formulário corretamente.')
+    }
+  }
+}
