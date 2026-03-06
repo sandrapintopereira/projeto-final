2e6f6a645011b0d6501c6c4d7cde725276944e73
Sandra Pereira <sandrajesuspintopereira@gmail.com>
2026-03-06 15:19:23 +0000

    feat: add footer component with styling and template

--- meu-projeto/src/app/app.ts
+++ meu-projeto/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { Aside } from './aside/aside'
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Aside],
+  imports: [RouterOutlet, Header, Aside, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
