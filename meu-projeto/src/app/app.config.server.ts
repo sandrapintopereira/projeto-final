d7d17437533d60ec9865afc959d8defa7aebfeee
Sandra Pereira <sandrajesuspintopereira@gmail.com>
2026-03-12 17:53:08 +0000

    feat: add multi-stage Dockerfile for Angular production build

--- meu-projeto/src/app/app.config.server.ts
+++ meu-projeto/src/app/app.config.server.ts
@@ -1,11 +1,11 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
-import { provideServerRendering, withRoutes } from '@angular/ssr';
+//import { provideServerRendering, withRoutes } from '@angular/ssr';
 import { appConfig } from './app.config';
-import { serverRoutes } from './app.routes.server';
+//import { serverRoutes } from './app.routes.server';
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering(withRoutes(serverRoutes))
+   // provideServerRendering(withRoutes(serverRoutes))
   ]
 };
 
